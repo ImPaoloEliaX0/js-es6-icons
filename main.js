15f509406507ae1cd79b2931375e97809d88af3f
Paolo Elia <92871203+ImPaoloEliaX0@users.noreply.github.com>
2022-02-07 19:32:04 +0100

    Esercizio svolto

---
+++ main.js
@@ -0,0 +1,154 @@
+const icons = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+const cardInject = document.getElementById('js-card');
+createIcons(cardInject, icons);
+
+function createIcons (cardInject, icons) {
+    let cardCreated = '';
+
+    icons.forEach(object => {
+
+        cardCreated += ` 
+                        <div class="card color-${object.color}">
+                          <i class="${object.family} ${object.prefix}${object.name}"></i>
+                          <span>${object.name}</span>
+                        </div>
+                        `
+
+    });
+    cardInject.innerHTML = cardCreated;
+
+}
+
+
+const selectType = document.getElementById('select-type');
+selectType.addEventListener('change', function () {
+
+    let selectValue = this.value;
+    if (selectValue == "all") {
+        createIcons(cardInject, icons);
+
+    } else {
+        const filterIcon = icons.filter(iconElement => {
+
+            if (iconElement.type == selectValue) {
+              return true;
+            } 
+              return false;
+        });
+        createIcons(cardInject, filterIcon);
+    }
+
+});
